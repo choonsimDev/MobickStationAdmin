d1512b6ccb8b8c32b3eb71ce2c1253037062e263
남정현 <namjeonghyeon@namjeonghyeon-ui-MacBookAir.local>
2024-04-20 13:28:47 +0900

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    body {
+        margin: 0;
+        padding: 0;
+    }
+    `;
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Styled from "styled-components";
+import Link from "next/link";
+// import SideBar from "@/components/sidebar";
+
+const StyledWrapper = Styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: flex-start;
+`;
+
+export default function Home() {
+  return <StyledWrapper>{/* <SideBar /> */}</StyledWrapper>;
+}
